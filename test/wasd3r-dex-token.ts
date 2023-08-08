8dd16b78fabf5d3e08264be30926933fb26526b5
Aaron <aaron@wasd3r.xyz>
2023-08-07 20:05:50 -0400

    add access control contract and test

--- test/wasd3r-dex-token.ts
+++ test/wasd3r-dex-token.ts
@@ -4,64 +4,54 @@ import hre from "hardhat"
 import { ZERO_ADDRESS } from "../src/constants"
 
 describe("Wasd3r AA Dex: Token Management", function () {
+  let suSigner
   let contract
 
   before("deploy the contract", async function () {
-    const contractFactory = await hre.ethers.getContractFactory("Wasd3rDexManager")
+    suSigner = (await hre.ethers.getSigners())[0]
+    const contractFactory = await hre.ethers.getContractFactory("Wasd3rDexManager", {
+      signer: suSigner,
+    })
     contract = await contractFactory.deploy()
     await contract.deployed()
   })
 
   it("Should have native token registered in the token manager", async function () {
     const nativeTokenKey = await contract.DEX_TOKEN_NATIVE_TOKEN_KEY()
-    expect(nativeTokenKey === "0:__native__:0:18")
+    expect(nativeTokenKey).to.equal("0:__native__:0:18")
 
     const nativeTokenInfo = await contract.dexTokens(nativeTokenKey)
-    expect(nativeTokenInfo.isValid)
-    expect(nativeTokenInfo.decimals === 18)
-    expect(nativeTokenInfo.tokenType === 0)
-    expect(nativeTokenInfo.tokenName === "__native__")
-    expect(Number(nativeTokenInfo.tokenId) === 0)
+    expect(nativeTokenInfo.isValid).to.equal(true)
+    expect(nativeTokenInfo.decimals).to.equal(18)
+    expect(nativeTokenInfo.tokenType).to.equal(0)
+    expect(nativeTokenInfo.tokenName).to.equal("__native__")
+    expect(nativeTokenInfo.tokenId).to.equal(BigInt("0"))
 
     const tokenInfo = await contract.getDexTokenInfo(nativeTokenKey)
-    expect(
-      // @ts-ignore
-      tokenInfo ===
-        {
-          isValid: true,
-          decimals: 18,
-          tokenType: 0,
-          tokenName: "__native__",
-          tokenId: BigInt("0"),
-        },
-    )
+    expect(tokenInfo.isValid).to.equal(true)
+    expect(tokenInfo.decimals).to.equal(18)
+    expect(tokenInfo.tokenType).to.equal(0)
+    expect(tokenInfo.tokenName).to.equal("__native__")
+    expect(tokenInfo.tokenId).to.equal(BigInt("0"))
 
-    expect(await contract.isValidDexToken(nativeTokenKey))
+    expect(await contract.isValidDexToken(nativeTokenKey)).to.equal(true)
   })
 
   it("Should handle non-registered token", async function () {
     const nativeTokenInfo = await contract.dexTokens("non-exist-token-key")
-    expect(!nativeTokenInfo.isValid)
-    expect(nativeTokenInfo.decimals === 0)
-    expect(nativeTokenInfo.tokenType === 0)
-    expect(nativeTokenInfo.tokenName === "")
-    expect(Number(nativeTokenInfo.tokenId) === 0)
+    expect(nativeTokenInfo.isValid).to.equal(false)
+    expect(nativeTokenInfo.decimals).to.equal(0)
+    expect(nativeTokenInfo.tokenType).to.equal(0)
+    expect(nativeTokenInfo.tokenName).to.equal("")
+    expect(Number(nativeTokenInfo.tokenId)).to.equal(0)
 
     expect(!(await contract.isValidDexToken("non-exist-token-key")))
-  })
-
-  it("Should deregister a token", async function () {
-    const nativeTokenKey = await contract.DEX_TOKEN_NATIVE_TOKEN_KEY()
-    expect(await contract.isValidDexToken(nativeTokenKey))
-
-    expect(await contract.deregisterDexToken(nativeTokenKey))
-    expect(!(await contract.isValidDexToken(nativeTokenKey)))
   })
 
   it("Should return a token key", async function () {
     const nativeTokenKey = await contract.DEX_TOKEN_NATIVE_TOKEN_KEY()
     const tokenKey1 = await contract.getTokenKey(0, ZERO_ADDRESS, 0, 18)
-    expect(nativeTokenKey === tokenKey1)
+    expect(nativeTokenKey).to.equal(tokenKey1)
 
     const tokenKey2 = await contract.getTokenKey(
       1, // ERC20
@@ -69,7 +59,8 @@ describe("Wasd3r AA Dex: Token Management", function () {
       0,
       6,
     )
-    expect(tokenKey2 === "1:0x75ce7AEE59347612ed29ff5c249e34ED1bc17D15:0:6")
+    // Contract address would be lowercase string in the token key.
+    expect(tokenKey2).to.equal("1:0x75ce7aee59347612ed29ff5c249e34ed1bc17d15:0:6")
   })
 
   it("Should register a token", async function () {
@@ -79,15 +70,37 @@ describe("Wasd3r AA Dex: Token Management", function () {
       0,
       6,
     )
-    expect(!(await contract.isValidDexToken(tokenKey)))
+    expect(await contract.isValidDexToken(tokenKey)).to.equal(false)
 
-    await contract.registerDexToken(
-      1, // ERC20
-      "0x75ce7AEE59347612ed29ff5c249e34ED1bc17D15", // token contract address
-      0,
-      6,
-      "USDT",
+    await expect(
+      contract.registerDexToken(
+        1, // ERC20
+        "0x75ce7AEE59347612ed29ff5c249e34ED1bc17D15", // token contract address
+        0,
+        6,
+        "USDT",
+      ),
     )
-    expect(await contract.isValidDexToken(tokenKey))
+      .to.emit(contract, "DexTokenRegistered")
+      .withArgs(
+        suSigner.address,
+        tokenKey,
+        "0x75ce7AEE59347612ed29ff5c249e34ED1bc17D15",
+        "USDT",
+        1,
+        6,
+        0,
+      )
+    expect(await contract.isValidDexToken(tokenKey)).to.equal(true)
+  })
+
+  it("Should deregister a token", async function () {
+    const nativeTokenKey = await contract.DEX_TOKEN_NATIVE_TOKEN_KEY()
+    expect(await contract.isValidDexToken(nativeTokenKey)).to.equal(true)
+
+    await expect(contract.deregisterDexToken(nativeTokenKey))
+      .to.emit(contract, "DexTokenDeregistered")
+      .withArgs(suSigner.address, nativeTokenKey)
+    expect(await contract.isValidDexToken(nativeTokenKey)).to.equal(false)
   })
 })
