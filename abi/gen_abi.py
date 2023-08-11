79c5568a19ba8cc87b18236ae73e59526796236d
Aaron <aaron@wasd3r.xyz>
2023-08-10 21:20:43 -0400

    add script to create abi files

---
+++ abi/gen_abi.py
@@ -0,0 +1,55 @@
+##!/usr/bin/env python3
+
+import json
+import os
+
+ENTRY_POINT_ABI_FILENAME = "EntryPoint.abi"
+DEX_MANAGER_ABI_FILENAME = "DexManager.abi"
+
+
+def gen_abi():
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    contract_build_dir = os.path.join(
+        current_dir,
+        "..",
+        "artifacts",
+        "contracts",
+        "aadex",
+    )
+
+    entrypoint_build_json_file = os.path.join(
+        contract_build_dir,
+        "Wasd3rDexEntryPoint.sol",
+        "Wasd3rDexEntryPoint.json",
+    )
+    dexmanager_build_json_file = os.path.join(
+        contract_build_dir,
+        "Wasd3rDexManager.sol",
+        "Wasd3rDexManager.json",
+    )
+
+    with open(entrypoint_build_json_file, "r") as file:
+        entrypoint_build_json = json.load(file)
+
+    with open(dexmanager_build_json_file, "r") as file:
+        dexmanager_build_json = json.load(file)
+
+    entrypoint_abi_file = os.path.join(
+        current_dir,
+        ENTRY_POINT_ABI_FILENAME,
+    )
+    dexmanager_abi_file = os.path.join(
+        current_dir,
+        DEX_MANAGER_ABI_FILENAME,
+    )
+
+    # Create EntryPoint.abi
+    with open(entrypoint_abi_file, "w") as file:
+        json.dump(entrypoint_build_json["abi"], file, indent=2)
+
+    # Create DexManager.abi
+    with open(dexmanager_abi_file, "w") as file:
+        json.dump(dexmanager_build_json["abi"], file, indent=2)
+
+
+gen_abi()
